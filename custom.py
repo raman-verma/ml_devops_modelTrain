e9ce56f1863d8738f8a14b3353c6401612eae771
raman <rverma7k@gmail.com>
2020-05-31 01:53:35 +0530

    change req

--- custom.py
+++ custom.py
@@ -1,35 +1,4 @@
-import numpy as np
-from keras.utils.np_utils import to_categorical # convert to one-hot-encoding
-from keras.models import Sequential
-from keras.layers import Dense, Flatten, Conv2D, MaxPool2D
-from keras.optimizers import Adam
 
-from keras.datasets import mnist
-dataset = mnist.load_data('mymnist.db')
-
-# data set contani both the things
-# Traing data
-# Test data
-train, test = dataset
-X_train, y_train = train
-X_test, y_test = test
-
-# Reshaping the the data for making it fit for the model traning.
-X_train = X_train.reshape(-1, 28, 28, 1)
-X_test = X_test.reshape(-1, 28, 28, 1)
-
-
-# we use the standard initialization methods for weights, 
-# however, data between 0 and 1 should make the net converge faster.
-X_train = X_train.astype("float32")/255.
-X_test = X_test.astype("float32")/255.
-
-# using one hot encoding for categorial data
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
-
-# Start building the model
-model = Sequential()
 
 model.add(Conv2D(filters = 32, kernel_size = (3, 3), activation='relu',
                  input_shape = (28, 28, 1)))
